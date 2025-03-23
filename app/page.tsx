3dd39c99b88361f76de8ad97a4aa33576b1e438c
Abhay Pratap Singh <138395064+Abhiolics@users.noreply.github.com>
2025-03-23 10:14:20 +0530

    errors fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 import React from 'react'
-import Home from './components/LandingPage'
-import IntegrationComponent from './components/integration'
-import PricingPlans from './components/pricing-plans'
+// import Home from './components/LandingPage'
+// import IntegrationComponent from './components/integration'
+// import PricingPlans from './components/pricing-plans'
 import LoginPage from './components/login'
 
 const page = () => {
